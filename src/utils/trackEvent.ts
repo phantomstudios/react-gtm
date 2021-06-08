a2195368cc12ed951185e754dcb78f5f07940b86
Paulo <mfjpaulo@gmail.com>
2021-06-08 21:12:23 +0100

    Fix undefined event not setting to default (#8)

--- src/utils/trackEvent.ts
+++ src/utils/trackEvent.ts
@@ -10,7 +10,7 @@ declare global {
 const trackEvent = (props?: EventTrackingProps) => {
   if (!IS_BROWSER || !window.dataLayer) return;
 
-  const event = props?.event ? "customEvent" : props?.event;
+  const event = !props?.event ? "customEvent" : props?.event;
   window.dataLayer = window.dataLayer || [];
   window.dataLayer.push({ event, ...props?.data });
 };
