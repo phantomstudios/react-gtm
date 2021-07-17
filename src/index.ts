c96c519fc11189940ae48f76587147cca24c4f77
Paulo <mfjpaulo@gmail.com>
2021-07-17 16:00:25 +0100

    Upgrade to GA4 (#14)
    * Upgrade to GA4
    
    * Change verison bump

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export { default as TrackingHeadScript } from "./components/TrackingHeadScript";
-export { default as TrackingBodyScript } from "./components/TrackingBodyScript";
 export { default as trackEvent } from "./utils/trackEvent";
 export {
   EmbedTrackingProps,
